8a0d18cf940af5d63aad9edee746139212a77f90
Kevin Farias <kevin.farias.rib@gmail.com>
2022-02-08 16:56:49 -0300

    2

---
+++ erros.js
@@ -0,0 +1,12 @@
+//const numero;
+
+// Missing initializer in const declaration
+// Inicializador ausente na declaração const
+
+const numero = 2;
+console.log(numero);
+
+//console.log(nome);
+//Reference Error: nome is not defined
+//Erro de referência: o nome não está definido
+
---
+++ tipagem.js
@@ -0,0 +1,9 @@
+//tipagem dinâmica
+
+//não é necessário dizer qual o tipo da variável
+
+var minhavar = 552;
+
+var minhavar = "Agora é string";
+
+var minhavar = true;
