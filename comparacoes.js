503f8d4c5ff14c24a4f5ecc4709bc83dda0362cf
Kevin Farias <kevin.farias.rib@gmail.com>
2022-02-10 10:49:44 -0300

    2

---
+++ comparacoes.js
@@ -0,0 +1,29 @@
+// == comparação implicita
+
+const num = 5;
+const text = "5";
+
+console.log(num == text)
+
+
+//typeof
+
+console.log(typeof num);
+console.log(typeof text);
+
+// == compara apenas o valor
+// === compara o valor e o tipo de dado
+
+//comparação explícita === (três = '===' )
+
+/*
+
+||: Operador “ou”, retorna true caso uma condição seja válida;
+
+
+&&: Operador “e”, retorna true somente se todas as condições forem válidas;
+
+
+!= e !==: Operadores “não igual” e “estritamente não igual”, utilizados para
+comparação, da mesma forma que == e === retornam true ou false.
+*/
