d86fa0d64f1dfcd185b77321e2e0dc310a250577
Kevin Farias <kevin.farias.rib@gmail.com>
2022-02-23 16:31:32 -0300

    2.0

---
+++ Fundamentos-02/add-nota.js
@@ -0,0 +1,9 @@
+// Desafio: Colocar a 4º nota
+const notas = [10, 6, 8];
+notas.push(7);
+console.log(notas);
+
+// PUSH = coloca elemento na última posição da array
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+console.log(media);
